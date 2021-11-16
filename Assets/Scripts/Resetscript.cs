bab1c451ade96c2f084b7498001d6acaf6edac2f
unini2525 <94427144+unini2525@users.noreply.github.com>
2021-11-16 15:51:09 +0900

    スクリプトの追加

---
+++ Assets/Scripts/Resetscript.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resetscript : MonoBehaviour
+{
+    [SerializeField]
+    private Button risetbutton_;
+
+    [SerializeField]
+    private Text valutext_;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if ()
+        {
+            Debug.Log("‚¨‚³‚ê‚½");
+        }
+    }
+
+
+}
